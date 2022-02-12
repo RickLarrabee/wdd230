65b81c3063d2d34e8d61fd0d6082e2a21de2fa84
RickLarrabee <lar21035@byui.edu>
2022-02-12 08:03:44 -0500

    update

---
+++ chamber/js/windchill.js
@@ -0,0 +1,15 @@
+let temp = 30;
+let windSpeed = 3.5;
+let windChill = '';
+
+if (temp <= 50 && windSpeed > 3.0) {
+    windChill = (35.74+(0.6215*temp)-(35.75*(windSpeed**0.16))+(0.4275*temp*(windSpeed**0.16))).toFixed(0);
+    document.querySelector('.windChill').innerHTML = `${windChill}&deg`
+}
+else {
+    windChill = 'N/A';
+    document.querySelector('.windChill').innerHTML = windChill
+}
+
+document.querySelector('.temp').innerHTML = `${temp}&deg`;
+document.querySelector('.windSpeed').innerHTML = windSpeed
