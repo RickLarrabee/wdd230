519411961b28b7157dd0ed738eefb05111d31f66
RickLarrabee <lar21035@byui.edu>
2022-02-11 22:48:00 -0500

    update

--- chamber/js/commerce.js
+++ chamber/js/commerce.js
@@ -15,3 +15,11 @@ hamButton.addEventListener('click', () => {mainNav.classList.toggle('responsive'
 const dateField = document.querySelector(".date");
 const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( d );
 dateField.innerHTML = `<em>${fullDate}</em>`;
+
+dayMessage = ''
+const weekday = 1;
+if (weekday > 0 && weekday < 3) {
+   dayMessage = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+}
+console.log(dayMessage)
+document.querySelector('.joinUsBanner').innerHTML = dayMessage
