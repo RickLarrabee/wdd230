fbf3576b3ee6521b84893c25f5a65165261129a7
RickLarrabee <lar21035@byui.edu>
2022-04-04 12:35:48 -0400

    update

--- templeinn/js/temple.js
+++ templeinn/js/temple.js
@@ -1,49 +1,62 @@
 const requestURL = "https://ricklarrabee.github.io/wdd230/templeinn/data/data.json";
 const temple_select = document.querySelector("#temple");
-let temple_data = temple_select.value
-console.log(temple_data)
+let temple_selected = temple_select.value;
+const cards = document.querySelector(".cards");
+const temple_data = []
+change_temple();
 
 temple_select.addEventListener ('change',(e)=> {
-    temple_data = '';
-    temple_data = temple_select.options[temple_select.selectedIndex].value;
-    console.log(temple_data);
+    temple_selected = '';
+    temple_selected = temple_select.options[temple_select.selectedIndex].value;
+    temple_data.length = 0;
+    cards.innerHTML = ''
+    change_temple();
 });
 
-function get_temple(temple) {
+function change_temple () {
   fetch(requestURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (jsonObject) {
-      console.table(jsonObject); // temporary checking for valid response and data parsing
+      console.table(jsonObject);
+      //console.table(jsonObject); // temporary checking for valid response and data parsing
       const temples = jsonObject["temples"];
-      temples.forEach(display_temples);
+      temples.forEach(temple_array);
     });
 
-  function display_temples(temple) {
-    // Create elements to add to the document
-    const cards = document.querySelector(".cards");
-    let card = document.createElement("section");
-    let h2 = document.createElement("h2");
-    let portrait = document.createElement("img");
-    let p = document.createElement("p");
-    let order = "";
+    function temple_array(temple) {
+      let temple_id = temple.id;
+      console.log(temple_selected)
+      if (temple_id == temple_selected) {
+        temple_data.push(temple)
+        console.table(temple_data)
+        temple_data.forEach(display_temple)
+      }
 
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = temple.name;
-    p.innerHTML = `address: ${temple.address} <br/> phone: ${temple.phone} <br/> email: ${temple.email}`;
+    function display_temple(temple) {
+      // Create elements to add to the document
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let portrait = document.createElement('img');
+      let p = document.createElement('p');
 
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute("src", temple.imgurl);
-    portrait.setAttribute("alt", `Portrait of ${temple.name}`);
-    portrait.setAttribute("loading", "lazy");
+      h2.textContent = temple.name;
+      p.innerHTML = `Address: ${temple.address} <br/> phone: ${temple.phone} <br/>
+      email: ${temple.email}`       
 
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(portrait);
-    card.appendChild(p);
+      // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+      portrait.setAttribute('src', temple.imgurl);
+      portrait.setAttribute('alt', `Portrait of ${temple.name}`);
+      portrait.setAttribute('loading', 'lazy');
+  
+      // Add/append the section(card) with the h2 element
+      card.appendChild(portrait);
+      card.appendChild(h2);
+      card.appendChild(p);
 
-    // Add/append the existing HTML div with the cards class with the section(card)
-    cards.appendChild(card);
+      // Add/append the existing HTML div with the cards class with the section(card)
+      cards.appendChild(card)
+    }
   }
 }
--- templeinn/js/weather.js
+++ templeinn/js/weather.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=4348599&units=imperial&APPID=96177d494514829db17df98e0f50bdce";
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=39.025669&lon=-77.07637&exclude=minutely,hourly&units=imperial&APPID=96177d494514829db17df98e0f50bdce";
 
 fetch(apiURL)
   .then((response) => response.json())
